07424cfcc6e25c75b32771f9b62a739810e6738d
Handi10 <Handiprasetia10@gmail.com>
2021-10-05 12:09:11 +0700

    initial commit

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,450 @@
+<html>
+
+<head>
+<title>Form input matakuliah</title>
+</head>
+<body>
+    <center>
+    <form action="<?= base_url("matakuliah/cetak"); ?>" 
+    method="post">
+         <table>
+             <tr>
+                 <th colspan="3">
+                 Form Input Data Mata Kuliah
+                 </th>
+             </tr>
+            <tr>
+                 <td colspan="3">
+                <hr>
+                 </td>
+            </tr>
+            <tr>
+                 <th>Kode Matakuliah</th>
+                 <th>:</th>
+                 <td>
+                 <Input type="text" name="kode" id="Kode">
+                </td>
+            </tr>
+            <tr>
+                 <th>Nama Matakuliah</th>
+                 <td>:</td>
+                 <td>
+                 <input type="text" name="nama" id="nama">
+                 </td>
+            </tr>
+            <tr>
+                 <th>SKS</th>
+                 <td>:</td>
+                 <td>
+                 <select name="Sks" id="Sks">
+                 <option value="">Pilih Sks</option>
+                 <option value="1">1</option>
+                 <option value="2">2</option>
+                 <option value="3">3</option>
+                 <option value="4">4</option>
+                 </select>
+                 </td>
+            <tr>
+                 <td colspan="3" align="center">
+                 <input type="submit" value="submit">
+                </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+               
+
+
+
+
+    </tr>
+    </form>
+
+</body>
